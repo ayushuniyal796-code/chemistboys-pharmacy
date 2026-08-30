0527ce34832ff1eedc25e3b02a1fbf2beab22f6c
Ayush Uniyal <ayushuniyal796@gmail.com>
2026-08-30 23:52:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,34 +1,48 @@
-// firebase.js
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import {
     getAuth,
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCiRX_njFBAAgUzM1vHDTEYgWk1TFLjcmQ",
+
+    apiKey: "AIzaSyCiRX_njFBAAgUzM1vHDTEYgWkT1FLjcmQ",
+
     authDomain: "chemistboys.firebaseapp.com",
+
     projectId: "chemistboys",
+
     storageBucket: "chemistboys.firebasestorage.app",
+
     messagingSenderId: "696067008650",
+
     appId: "1:696067008650:web:aba739ed1593d315002573",
+
     measurementId: "G-G3BHP0PSB0"
+
 };
+
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 
-// Firebase ka auth state ready hone ka wait
+
 export const authReady = new Promise((resolve) => {
 
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(
+        auth,
+        () => {
 
-        resolve(user);
+            unsubscribe();
 
-        unsubscribe();
+            resolve();
 
-    });
+        }
+    );
 
 });
