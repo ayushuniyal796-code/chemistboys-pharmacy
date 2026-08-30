c090ef13d72cd050abb06954ef8bdd06db00ea08
Ayush Uniyal <ayushuniyal796@gmail.com>
2026-08-30 21:16:30 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCiRX_njFBAAgUzM1vHDTEYgWk1TFLjcmQ",
+    authDomain: "chemistboys.firebaseapp.com",
+    projectId: "chemistboys",
+    storageBucket: "chemistboys.firebasestorage.app",
+    messagingSenderId: "696067008650",
+    appId: "1:696067008650:web:aba739ed1593d315002573",
+    measurementId: "G-G3BHP0PSB0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
