e2d89ef17b9db38a2a31dc47e72309ba342f6b2c
Ayush Uniyal <ayushuniyal796@gmail.com>
2026-09-03 00:06:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,6 +7,10 @@ import {
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyCiRX_njFBAAgUzM1vHDTEYgWk1TFLjcmQ",
@@ -19,19 +23,27 @@ const firebaseConfig = {
 };
 
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+
+// Firebase Authentication
 export const auth = getAuth(app);
 
 
+// Cloud Firestore
+export const db = getFirestore(app);
+
+
+// Wait until Firebase knows the current login state
 export const authReady = new Promise((resolve) => {
 
-    const unsubscribe = onAuthStateChanged(
-        auth,
-        () => {
-            unsubscribe();
-            resolve();
-        }
-    );
+    const unsubscribe = onAuthStateChanged(auth, () => {
+
+        unsubscribe();
+
+        resolve();
+
+    });
 
 });
